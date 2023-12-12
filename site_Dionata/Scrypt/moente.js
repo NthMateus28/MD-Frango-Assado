8fb97e64be08c545055760b860290e53ef83039f
Mateus Loeser Reisdorfer <79878580+NthMateus28@users.noreply.github.com>
2023-12-12 14:39:31 -0300

    Update moente.js

--- site_Dionata/Scrypt/moente.js
+++ site_Dionata/Scrypt/moente.js
@@ -1,10 +1,78 @@
 document.addEventListener("DOMContentLoaded", function () {
     const products = document.querySelectorAll(".product");
     const valorFinalElement = document.querySelector(".resultadoFinal");
-    const precos = [45, 55, 45, 50, 48, 5, 3, 2, 6, 6, 4.50, 14, 15, 12];
+    const precos = [45, 55, 45, 50, 48, 5, 3, 2, 6, 6, 4.5, 14, 15, 12];
+
+    // Sua configuração do Firebase
+    const firebaseConfig = {
+        apiKey: "AIzaSyBHtmcLp__TN7bJuc57P5hVYEXQxxmIPO4",
+        authDomain: "md-franfo-assado.firebaseapp.com",
+        databaseURL: "https://md-franfo-assado-default-rtdb.firebaseio.com",
+        projectId: "md-franfo-assado",
+        storageBucket: "md-franfo-assado.appspot.com",
+        messagingSenderId: "437304446925",
+        appId: "1:437304446925:web:b60110b532736da8867e84",
+        measurementId: "G-2DSPDTRXSE",
+    };
+
+    // Inicialize o app Firebase
+    const app = firebase.initializeApp(firebaseConfig);
+    const database = app.database(); // Inicialize o Realtime Database
+
+    // Função para carregar a disponibilidade dos produtos do Firebase
+    function carregarDisponibilidade() {
+        const disponibilidadeRef = database.ref("produtos");
+        disponibilidadeRef.on(
+            "value",
+            (snapshot) => {
+                const disponibilidadeProdutos = snapshot.val();
+                atualizarVisibilidade(disponibilidadeProdutos);
+            },
+            (errorObject) => {
+                console.error("Erro ao carregar disponibilidade:", errorObject);
+            }
+        );
+    }
+
+    // Atualizar visibilidade dos produtos com base na disponibilidade
+    function atualizarVisibilidade(disponibilidadeProdutos) {
+        products.forEach((product, index) => {
+            const tarja = document.createElement("div");
+            tarja.classList.add("tarja");
+
+            if (
+                disponibilidadeProdutos &&
+                disponibilidadeProdutos[index + 1] &&
+                disponibilidadeProdutos[index + 1].disponibilidade === false
+            ) {
+                const buttonContainer =
+                    product.querySelector(".button-container");
+                const btnAdicionar = product.querySelector(".btnAdicionar");
+                const btnRetirar = product.querySelector(".btnRetirar");
+                const titleProduct = product.querySelector(".titleProduct");
+
+                product.appendChild(tarja);
+                buttonContainer.style.display = "none";
+                titleProduct.childNodes[2].textContent = "Indisponível";
+                titleProduct.childNodes[3].style.display = "none";
+                titleProduct.childNodes[4].style.display = "none";
+                titleProduct.childNodes[5].style.display = "none";
+
+                btnAdicionar.removeEventListener("click", () => {});
+                btnRetirar.removeEventListener("click", () => {});
+            } else {
+                const tarja = product.querySelector(".tarja");
+                if (tarja) {
+                    tarja.remove();
+                }
+            }
+        });
+    }
+
+    // Chamada para carregar a disponibilidade dos produtos
+    carregarDisponibilidade();
 
     localStorage.clear();
-
 
     function calcularValorTotal() {
         let valorTotal = 0;
@@ -53,26 +121,49 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 
-    products.forEach((product, index) => {
-        const btnAdicionar = product.querySelector(".btnAdicionar");
-        const btnRetirar = product.querySelector(".btnRetirar");
-        const unidade = product.querySelector(".unidade");
+    // Desabilita todos os produtos indisponíveis de uma vez
+    function desabilitarProdutosIndisponiveis(disponibilidadeProdutos) {
+        products.forEach((product, index) => {
+            const tarja = product.querySelector(".tarja");
+            if (
+                disponibilidadeProdutos &&
+                disponibilidadeProdutos[index + 1] &&
+                disponibilidadeProdutos[index + 1].disponibilidade === false
+            ) {
+                if (!tarja) {
+                    const buttonContainer =
+                        product.querySelector(".button-container");
+                    const btnAdicionar = product.querySelector(".btnAdicionar");
+                    const btnRetirar = product.querySelector(".btnRetirar");
+                    const titleProduct = product.querySelector(".titleProduct");
 
-        btnAdicionar.addEventListener("click", () => {
-            let valorAtual = parseInt(unidade.textContent);
-            unidade.textContent = valorAtual + 1;
+                    const tarja = document.createElement("div");
+                    tarja.classList.add("tarja");
+                    product.appendChild(tarja);
 
-            salvarProduto(index, valorAtual + 1);
-            atualizarValorFinal();
-        });
+                    buttonContainer.style.display = "none";
+                    titleProduct.childNodes[2].textContent = "Indisponível";
+                    titleProduct.childNodes[3].style.display = "none";
+                    titleProduct.childNodes[4].style.display = "none";
+                    titleProduct.childNodes[5].style.display = "none";
 
-        btnRetirar.addEventListener("click", () => {
-            let valorAtual = parseInt(unidade.textContent);
-            if (valorAtual > 0) {
-                unidade.textContent = valorAtual - 1;
-                salvarProduto(index, valorAtual - 1);
-                atualizarValorFinal();
+                    btnAdicionar.removeEventListener("click", () => {});
+                    btnRetirar.removeEventListener("click", () => {});
+                }
+            } else {
+                if (tarja) {
+                    tarja.remove();
+                }
             }
         });
+    }
+
+    // Inicialmente, desabilita os produtos indisponíveis
+    carregarDisponibilidade();
+
+    // Evento para atualizar a disponibilidade dos produtos
+    database.ref("produtos").on("value", (snapshot) => {
+        const disponibilidadeProdutos = snapshot.val();
+        desabilitarProdutosIndisponiveis(disponibilidadeProdutos);
     });
 });
